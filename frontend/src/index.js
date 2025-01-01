272d25d47ec5e1035799d8279ddff93c47215ace
Ashutosh Prajapati <ashutosh1205vns@gmail.com>
2025-01-02 00:58:39 +0530

    Hero Write

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import HomePage from './landingPage/home/HomePage';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <div></div>
-  </React.StrictMode>
+  <HomePage />
 );
 
---
+++ frontend/src/landingPage/home/HomePage.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import Hero from './Hero'   // Import the Hero component from the same directory
+function HomePage() {
+    return ( 
+        <Hero/>
+     );
+}
+
+export default HomePage;

36aff627b198902fcd4db271400529a33c8f58b3
Ashutosh Prajapati <ashutosh1205vns@gmail.com>
2025-01-05 03:13:04 +0530

    fourthDay

--- frontend/src/landingPage/Footer.js
+++ frontend/src/landingPage/Footer.js
@@ -1,8 +1,28 @@
 import React from 'react'
 function Footer() {
     return ( 
-        <h1>Footer</h1>
-     );
+        <div className='container mainfooter'>
+            <div className='row footer'>
+                <div className='col-12 col-md-4 text-center mt-5'>
+                    <h6>HELPFUL LINKS</h6>
+                    <a href="">My Linkedin</a>
+                    <a></a>
+                </div>
+                <div className='col-12 col-md-4 mt-5'>
+                    <h6>GET IN TOUCH</h6>
+                    <a href="">ashutosh1205vns.@gmail.com</a><br/>
+                    <a href="">ashutosh7355vns@gmail.com</a>
+                </div>
+                <div className='col-12 col-md-4 mt-5'>
+                    <h6>CONNECT WITH US</h6>
+                    <a href="">insta link</a><br/>
+                    <a href="">facebook link</a><br/>
+                    <a href="">twitter link</a><br/>
+                    <a href="">linkedin link</a><br/>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default Footer;
--- frontend/src/landingPage/home/Hero.js
+++ frontend/src/landingPage/home/Hero.js
@@ -1,8 +1,44 @@
-import React from 'react'
+import './Hero.css';
+
 function Hero() {
+    const removeListItemDots = () => {
+        const style = document.createElement('style');
+        style.innerHTML = `
+            ul {
+                list-style-type: none;
+                padding: 0;
+            }
+        `;
+        document.head.appendChild(style);
+    };
+
+    removeListItemDots();
     return ( 
         <>
-        <h1>HomePage</h1>
+        <div className='container hero'>
+            <div className='row mt-5'>
+                <div className='col-12 col-md-8 text-center text-md-left'>
+                    <h1>Hello</h1>
+                    <h1>I'm Ashutosh Prajapati</h1>
+                    <h3>A Student of <br></br> National Institute Of Technology Patna</h3>
+                    <br/>
+                    <ul>
+                    <li>Proficient in Data Structrue and Algorithim<br/>
+                    Competitive Programmer</li>
+                    <li>Full Stack (MERN) Developer</li>
+                    <li>Proficient in C, C++, JAVA, Python </li>
+                    </ul>
+                </div>
+                <div className='col-12 col-md-8 text-center ashutosh'>
+                    <img src='/media/images/ashu.png' alt="Ashutosh Prajapati"></img>
+                </div>
+            </div>
+            <div className='row mt-5 signdiv'>
+                <button className='sign'>SignUp</button>
+                <h5 className='make'>I can make a Website Fastly using MONGO DB, EXPRESS JS, REACT JS, NODE JS</h5>
+            </div>
+            
+        </div>
         </>
      );
 }
